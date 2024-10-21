785dec8669b9b67cba5bcdb976a06ae355aa2754
Tam107 <97619178+Tam107@users.noreply.github.com>
2024-10-21 09:49:42 +0700

    Update boot_crud_db.sql

--- database/boot_crud_db.sql
+++ database/boot_crud_db.sql
@@ -1,24 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Feb 24, 2024 at 03:34 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `boot_crud_db`
+-- Database: `bookdb`
 --
 
 -- --------------------------------------------------------
